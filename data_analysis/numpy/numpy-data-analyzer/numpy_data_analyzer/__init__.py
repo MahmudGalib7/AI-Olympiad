29807e2a08a966d742e80420371ff5d47b1d0aa9
Mahmud Galib <mahmudgalib2009@gmail.com>
2025-03-04 22:45:15 +0600

    Removed numpy-data-analyzer from the index

--- data_analysis/numpy/numpy-data-analyzer/numpy_data_analyzer/__init__.py
+++
@@ -1,4 +0,0 @@
-"""NumPy Data Analyzer package."""
-from .data_processor import DataAnalyzer
-
-__all__ = ['DataAnalyzer']
--- data_analysis/numpy/numpy-data-analyzer/src/utils.py
+++
@@ -1,8 +0,0 @@
-def calculate_mean(data):
-    return np.mean(data)
-
-def calculate_median(data):
-    return np.median(data)
-
-def calculate_std_dev(data):
-    return np.std(data)
