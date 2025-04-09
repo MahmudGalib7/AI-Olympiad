699f0393a7d3a901f38a415adcb8a55deee62e70
Master Fru <wisemanmaster123@gmail.com>
2025-04-09 23:32:42 +0600

    Added every important files

--- python/DSA/learning_stack_queue.py
+++ python/DSA/learning_stack_queue.py
@@ -20,32 +20,6 @@
 # is_stack_empty = not bool(stack)
 # print(is_stack_empty)
 
-# OOP -> demo
-
-# class Car:
-#     def __init__(self, color, model, car_name):
-#         self.color = color
-#         self.model = model
-#         self.car_name = car_name
-#         self.speed = 0
-#
-#     def start(self):
-#         return f"{self.color} color {self.model} model {self.car_name} is starting..."
-#
-#     def a(self, speed):
-#         self.speed += speed
-#         return self.speed
-#
-# bmw_color = "Blue"
-# bmw_model = "A1"
-# bmw_car_name = "BMW"
-#
-# BMW = Car(bmw_color, bmw_model, bmw_car_name)
-# print(BMW.color)
-# print(BMW.model)
-# print(BMW.start())
-# print(BMW.a(60))
-
 # stack class
 
 # class Stack:
@@ -73,50 +47,121 @@
 #     def size(self):
 #         return len(self.stack)
 
+# class Node:
+#     def __init__(self, value):
+#         self.value = value
+#         self.next = None
+#
+#
+# class Stack:
+#     def __init__(self):
+#         self.head = None
+#         self.size = 0
+#
+#     def push(self, value):
+#         new_node = Node(value)
+#         if self.head:
+#             new_node.next = self.head
+#         self.head = new_node
+#         self.size += 1
+#
+#     def pop(self):
+#         if self.isEmpty():
+#             return "Stack is empty"
+#         popped_node = self.head
+#         self.head = self.head.next
+#         self.size -= 1
+#         return popped_node.value
+#
+#     def peek(self):
+#         if self.isEmpty():
+#             return "Stack is empty"
+#         return self.head.value
+#
+#     def isEmpty(self):
+#         return self.size == 0
+#
+#     def stackSize(self):
+#         return self.size
+#
+#
+# myStack = Stack()
+# myStack.push('A')
+# myStack.push('B')
+# myStack.push('C')
+#
+# print("Pop: ", myStack.pop())
+# print("Peek: ", myStack.peek())
+# print("isEmpty: ", myStack.isEmpty())
+# print("Size: ", myStack.stackSize())
+
+# Updated Version with less flaws
+
 class Node:
-    def __init__(self, value):
+    def __init__(self, value: any):
         self.value = value
-        self.next = None
+        self.next: 'Node | None' = None
+
+    def __repr__(self):
+        return f"Node({self.value})"
 
 
 class Stack:
     def __init__(self):
-        self.head = None
-        self.size = 0
+        self.head: Node | None = None
+        self.size: int = 0
 
-    def push(self, value):
+    def push(self, value: any) -> None:
+        self.head = Node(value)
+        self.head.next = self.head if self.size else None
+        self.head.next = self.head.next if self.size else None
+        self.head.next = self.head if self.size else None
+        self.head.next = self.head.next if self.size else None
         new_node = Node(value)
-        if self.head:
-            new_node.next = self.head
+        new_node.next = self.head
         self.head = new_node
         self.size += 1
 
-    def pop(self):
-        if self.isEmpty():
-            return "Stack is empty"
+    def pop(self) -> any:
+        if not self.head:
+            raise IndexError("Pop from empty stack")
         popped_node = self.head
         self.head = self.head.next
         self.size -= 1
         return popped_node.value
 
-    def peek(self):
-        if self.isEmpty():
-            return "Stack is empty"
+    def peek(self) -> any:
+        if not self.head:
+            raise IndexError("Peek from empty stack")
         return self.head.value
 
-    def isEmpty(self):
-        return self.size == 0
+    def is_empty(self) -> bool:
+        return self.head is None
 
-    def stackSize(self):
+    def __len__(self):
         return self.size
 
+    def __bool__(self):
+        return not self.is_empty()
 
-myStack = Stack()
-myStack.push('A')
-myStack.push('B')
-myStack.push('C')
+    def __str__(self):
+        elements = []
+        current = self.head
+        while current:
+            elements.append(repr(current.value))
+            current = current.next
+        return "Stack(top -> bottom): " + " -> ".join(elements)
 
-print("Pop: ", myStack.pop())
-print("Peek: ", myStack.peek())
-print("isEmpty: ", myStack.isEmpty())
-print("Size: ", myStack.stackSize())
+
+# Example usage
+if __name__ == "__main__":
+    my_stack = Stack()
+    my_stack.push('A')
+    my_stack.push('B')
+    my_stack.push('C')
+
+    print(my_stack)
+    print("Pop:", my_stack.pop())
+    print("Peek:", my_stack.peek())
+    print("isEmpty:", my_stack.is_empty())
+    print("Size:", len(my_stack))
